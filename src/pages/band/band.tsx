9922ee092965857287cd30bbb428483ebd3e3a66
Luiz Claudio <luiz.lclr@gmail.com>
2023-10-05 01:53:35 -0300

    feat(band page): add call to getBandById

--- src/pages/band/band.tsx
+++ src/pages/band/band.tsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 import { Title } from "~/components/title/title";
+import { Band } from "~/entities/band";
+import { useApp } from "~/state/app/hook";
 
-const Band: React.FC = () => <Title>Band</Title>;
+const BandPage: React.FC = () => {
+  const { id } = useParams();
+  const { bandService, isLoading, setIsLoading } = useApp();
+  const [band, setBand] = useState<Band | null>(null);
 
-export default Band;
+  useEffect(() => {
+    getBand().catch(() => {});
+  }, []);
+
+  const getBand = async (): Promise<void> => {
+    if (!id) return;
+
+    setIsLoading(true);
+
+    await bandService.getBandById(id, {
+      onSuccess,
+      onNotFound: () => {},
+      onError: () => {},
+    });
+
+    setIsLoading(false);
+  };
+
+  const onSuccess = (band: Band): void => {
+    setBand(band);
+    setIsLoading(false);
+  };
+
+  return <>{!isLoading && <Title>{band?.name}</Title>}</>;
+};
+
+export default BandPage;
